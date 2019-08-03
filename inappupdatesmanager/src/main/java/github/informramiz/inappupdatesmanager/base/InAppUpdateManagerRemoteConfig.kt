25bc895bc0ce716c5f213d00bfb935457e9a6d26
Ramiz Raja <informramiz@gmail.com>
2019-08-04 02:47:21 +0500

    feat: Write remote config handler

---
+++ inappupdatesmanager/src/main/java/github/informramiz/inappupdatesmanager/base/InAppUpdateManagerRemoteConfig.kt
@@ -0,0 +1,42 @@
+package github.informramiz.inappupdatesmanager.base
+
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import com.google.gson.Gson
+import github.informramiz.inappupdatesmanager.common.fromJson
+import github.informramiz.inappupdatesmanager.models.UpdateRemoteConfigInfo
+import timber.log.Timber
+
+
+/**
+ * Created by Ramiz Raja on 2019-05-30.
+ */
+internal object InAppUpdateManagerRemoteConfig {
+    var updateRemoteConfigKey = "androidSitterUpdateConfig"
+    var fetchInterval: Long = 6 * 60 * 60 //12 hours
+
+    fun doImmediateUpdate(versionCode: Int): Boolean {
+        val remoteConfigStr = FirebaseRemoteConfig.getInstance().getString(updateRemoteConfigKey)
+        val remoteConfigInfo = Gson().fromJson<UpdateRemoteConfigInfo>(remoteConfigStr)
+
+        return (versionCode >= remoteConfigInfo.versionCode
+                && remoteConfigInfo.isForceUpdate)
+    }
+
+    fun fetchAndActivate() {
+        val fetchAndActivateTask = getFirebaseRemoteConfig().fetchAndActivate()
+        fetchAndActivateTask.addOnCompleteListener { fetchResult ->
+            Timber.d("Remote Config: ${FirebaseRemoteConfig.getInstance().getString(updateRemoteConfigKey)}")
+        }
+    }
+
+    private fun getFirebaseRemoteConfig(): FirebaseRemoteConfig {
+        return FirebaseRemoteConfig.getInstance().apply {
+            val configSettings = FirebaseRemoteConfigSettings.Builder()
+                //12 hours
+                .setMinimumFetchIntervalInSeconds(fetchInterval)
+                .build()
+            this.setConfigSettingsAsync(configSettings)
+        }
+    }
+}
