b50eb29c066b9d08f74c39caf54b61ee934978d6
Ramiz Raja <informramiz@gmail.com>
2019-08-04 03:07:11 +0500

    refactor: Make some classes internal

--- inappupdatesmanager/src/main/java/github/informramiz/inappupdatesmanager/common/GsonExtension.kt
+++ inappupdatesmanager/src/main/java/github/informramiz/inappupdatesmanager/common/GsonExtension.kt
@@ -7,7 +7,7 @@ import com.google.gson.reflect.TypeToken
 /**
  * Created by Ramiz Raja on 2019-08-04.
  */
-inline fun <reified T> Gson.fromJson(string: String): T {
+internal inline fun <reified T> Gson.fromJson(string: String): T {
     //Java does not allow generic types retrieval at runtime
     //and because Gson using reflection so we need type information at runtime
     //so we make a subclass of TypeToken using which we can retrieve type info at runtime
--- inappupdatesmanager/src/main/java/github/informramiz/inappupdatesmanager/models/UpdateRemoteConfigInfo.kt
+++ inappupdatesmanager/src/main/java/github/informramiz/inappupdatesmanager/models/UpdateRemoteConfigInfo.kt
@@ -4,5 +4,5 @@ package github.informramiz.inappupdatesmanager.models
 /**
  * Created by Ramiz Raja on 2019-08-04.
  */
-data class UpdateRemoteConfigInfo(val versionCode: Int,
+internal data class UpdateRemoteConfigInfo(val versionCode: Int,
                                  val isForceUpdate: Boolean)
